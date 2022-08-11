a0ad2d386b11424e0fd567031de9417955113d35
Wilander Velazco <wilandervelazco@gmail.com>
2022-08-11 14:48:55 -0400

    comentario

---
+++ desafio15Clase34/src/passport.js
@@ -0,0 +1,8 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const ClassUserMDB = require('../public/ClaseUsuariosMDB.js')
+
+const user = new ClassUserMDB()
+
+
+
