52e455fd8afe94dd2879423e9f0044017407aabd
Wilander Velazco <wilandervelazco@gmail.com>
2022-09-11 17:56:43 -0300

    los test funcionando

--- desafio18Clase42/test/server.test.js
+++ desafio18Clase42/test/server.test.js
@@ -1,35 +1,78 @@
+//const app = require('../src/server.js')
 const { strictEqual, deepStrictEqual } = require('assert')
-const request = require('supertest')
+const request = require('supertest')('http://localhost:8080')
 const axios = require('axios')
+const expect = require('chai').expect
 
-const urlPostGet = 'http://localhost:8080/api/productos'
-const urlById = 'http://localhost:8080/api/productos/1'
+const postProd = producto => axios.post('http://localhost:8080/api/productos', {producto})
+const getProducto = () => axios('http://localhost:8080/api/productos')
 
 describe("Comprobando que el servidor de productos funcione", function() {
     beforeEach(async function(){
-        const user = {
-            username: "wilandervelazco@gmail.com",
-            password: "pepe"
-        }
-        await axios.post('http://localhost:8080/api/login', user)
         console.log('------Inicio del test------')
     })
 
-    it ("Comprobar agregar un producto", async function (){
+    it ("Verificar POST productos", async function (){
         const producto = {
-            title: 'producto 1',
-            price: 1234,
+            title: 'producto original',
+            price: 1111,
             thumbnail: 'foto de prueba'
         }
-        await axios.post(urlPostGet, producto, config)
-        .then((res) =>{
-            console.log(res)
-        }).catch((err) => {
-            console.log(err)
-        })
+        const res = await request.post('/api/productos').send(producto)
+        console.log(res.statusCode)
+        //expect(res.body.title).to.eql(producto.title)
+        //expect(res.body.price).to.eql(producto.price)
+        //expect(res.body.thumbnail).to.eql(producto.thumbnail)
+        expect(res.statusCode).to.eql(302)
+
+    })
+
+    it ("Verificar funcion GET productos", async function (){
+        const res = await request.get('/api/productos')
+        console.log(res.statusCode)
+        expect(res.statusCode).to.eql(200)
+
+    })
+
+    it ("Verificar GET por id", async function (){
+        const res = await request.get('/api/productos/1')
+        //console.log(res.body)
+        console.log(res.statusCode)
+        //expect(res.body.title).to.eql(producto.title)
+        //expect(res.body.price).to.eql(producto.price)
+        //expect(res.body.thumbnail).to.eql(producto.thumbnail)
+        expect(res.statusCode).to.eql(200)
+
+    })
+
+    it ("Verificar el PUT de producto", async function (){
+        const producto = {
+            "title": 'producto cambiado',
+            "price": 1234,
+            "thumbnail": 'foto de prueba cambiada'
+        }
+        const res = await request.put('/api/productos/1').send(producto)
+        console.log(res.body)
+        console.log(res.statusCode)
+        //expect(res.body.title).to.eql(producto.title)
+        //expect(res.body.price).to.eql(producto.price)
+        //expect(res.body.thumbnail).to.eql(producto.thumbnail)
+        expect(res.statusCode).to.eql(302)
+
+    })
+
+    it ("Verificar el DELETE de producto", async function (){
+        const res = await request.delete('/api/productos/1')
+        console.log(res.body)
+        console.log(res.statusCode)
+        //expect(res.body.title).to.eql(producto.title)
+        //expect(res.body.price).to.eql(producto.price)
+        //expect(res.body.thumbnail).to.eql(producto.thumbnail)
+        expect(res.statusCode).to.eql(302)
+
     })
 
     afterEach(function() {
-        console.log('------- Fin TOTAL de Test -------')
+        console.log('------- Fin de Test -------')
     })
 })
