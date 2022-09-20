c07dfbc085e024e26bf5a444d2ca436c4849f259
Wilander Velazco <wilandervelazco@gmail.com>
2022-09-19 23:14:46 -0300

    terminando el desafio con sails

---
+++ desafio20Clase46/api/models/Productos.js
@@ -0,0 +1,16 @@
+/**
+ * Productos.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    title: {type: 'string', required: true},
+    price: {type: 'number', required: true},
+    thumbnail: {type: 'string', required: true} 
+  },
+};
+
